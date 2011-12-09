fcdbf26b7c839ed581f4ce915af40686b13e4b14
Kriss Watt <kriss.watt@gmail.com>
2011-12-09 14:47:39 +0000

    Refactored code that pulls data in and added support for Guardian RSS feeds.

--- app.py
+++ app.py
@@ -1,65 +1,139 @@
-from flask import Flask, render_template, abort
+from flask import Flask, make_response, render_template, abort, redirect, url_for
 import requests
 import random
 import json
+import feedparser
 
 CONTENT_API = 'http://content.guardianapis.com/search'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-@app.route('/readnext', methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
-    """Index view."""
+    """Index view. Currently just redirects to the only available component."""
+    return redirect(url_for('readnext'))
+
+@app.route('/readnext', methods=['GET'])
+def readnext():
+    """Read next component."""
     feeds = []
-    add_api_feed(feeds, 'Latest news', 'news')
-    add_api_feed(feeds, 'Latest comment', 'commentisfree')
-    add_api_feed(feeds, 'Latest sport', 'sport')
-    add_api_feed(feeds, 'Latest music', 'music')
+    
+    # API feeds
+    api = ContentApi(feeds)
+    api.add_feed('Latest news', 'news')
+    api.add_feed('Latest comment', 'commentisfree')
+    api.add_feed('Latest sport', 'sport')
+    api.add_feed('Latest music', 'music')
+    
+    # RSS feeds
+    rss = RssFeeds(feeds)
+    rss.add_feed('World lead stories', 'http://www.guardian.co.uk/world/lead/rss')
+    rss.add_feed('Environment lead stories', 'http://www.guardian.co.uk/environment/lead/rss')
+    
     random.shuffle(feeds)
     if not feeds:
         abort(404)
-    return render_template('index.html', feeds=feeds)
+    
+    response = make_response(render_template('index.html', feeds=feeds))
+    return response
 
-def add_api_feed(feeds, title, section):
-    """Adds a feed to be obtained and outputted."""
-    feed = process_json(get_articles_for_section(section))
-    if feed:
-        feeds.append({'title': title, 'articles': feed})
 
-def get_articles_for_section(section):
-    """Shortcut method to preconfigure most of the API parameters."""
-    url = '?section=%s&page-size=3&format=json&show-fields=thumbnail'
-    return call_content_api(url % section)
-
-def call_content_api(url):
-    """Attempts to retrieve data from the Content API."""
-    r = requests.get(app.config['CONTENT_API'] + url)
-    if r.status_code == 200:
-        return r.content
-    return False
-
-def process_json(data):
-    """Process JSON so that it can be used by the template context."""
-    if data:
-        data = json.loads(data)
-        results = []
-        for result in data['response']['results']:
-            results.append(build_article_dict(result))
-        return results
-    return False
-
-def build_article_dict(json):
-    """Parses supplied JSON to build a dictionary for an article."""
-    article = {
-        'title': json['webTitle'],
-        'url': json['webUrl'],
-    }
-    try:
-        article['thumbnail'] = json['fields']['thumbnail']
-    except KeyError:
+class DataGrabber:
+    """
+    Common methods required for getting and parsing data.
+    
+    This is here largely to act as a common interface for building classes
+    for accessing different types of data from various sources.
+    """
+    
+    def __init__(self, feeds):
+        """Class constructor."""
+        self.feeds = feeds
+    
+    def add_feed(self, title, key):
+        """Add a feed to be grabbed."""
+        feed = self.process_data(self.get_data(key))
+        if feed:
+            self.feeds.append({'title': title, 'feed': feed})
+    
+    def get_data(self, key):
+        """Attempt to retrieve data."""
+        r = requests.get(key)
+        if r.status_code == 200:
+            return r.content
+        return False
+    
+    def process_data(self, data):
+        """Process retrieved data."""
         pass
-    return article
+    
+    def build_item(self, data):
+        """Build a dictionary to use in the template context."""
+        pass
+
+
+class RssFeeds(DataGrabber):
+    """Methods to request and process Guardian RSS feed data."""
+    ITEMS_TO_SHOW = 3
+    
+    def process_data(self, data):
+        """Process Guardian RSS feed data."""
+        if data:
+            rss_data = feedparser.parse(data)
+            items = []
+            i = 0
+            for entry in rss_data.entries:
+                if i < self.ITEMS_TO_SHOW:
+                    items.append(self.build_item(entry))
+                    i = i + 1
+                else:
+                    break
+            return items
+        return False
+    
+    def build_item(self, data):
+        """Transform RSS data into a dictionary that the template can use."""
+        article = {
+            'title': data['title'],
+            'url': data['link'],
+        }
+        return article
+
+
+class ContentApi(DataGrabber):
+    """Methods to request and process Content API data."""
+    API_PARAMS = '%s?section=%s&page-size=3&format=json&show-fields=thumbnail'
+    
+    def add_feed(self, title, key):
+        """Add an API feed using a set of predetermined parameters."""
+        url =  self.API_PARAMS % (app.config['CONTENT_API'], key)
+        feed = self.process_data(self.get_data(url))
+        if feed:
+            self.feeds.append({'title': title, 'feed': feed})
+
+    def process_data(self, data):
+        """Process Content API JSON data."""
+        if data:
+            json_data = json.loads(data)
+            items = []
+            for result in json_data['response']['results']:
+                items.append(self.build_item(result))
+            return items
+        return False
+
+    def build_item(self, data):
+        """Transform JSON data into a dictionary that the template can use."""
+        article = {
+            'title': data['webTitle'],
+            'url': data['webUrl'],
+        }
+        try:
+            article['thumbnail'] = data['fields']['thumbnail']
+        except KeyError:
+            pass
+        return article
+
 
 if __name__ == "__main__":
     app.run(debug=True)
